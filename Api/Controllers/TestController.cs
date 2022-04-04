e031a7fdc8857b2c495bc2a0e573180a6518445b
Alexander <a89good@gmail.com>
2022-04-04 23:33:35 +0300

    wip

---
+++ Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {        
+        [HttpGet(Name = "test1")]
+        public async Task<IEnumerable<string>> Test1()
+        {
+            return await Task.FromResult(new HashSet<string>{ "test" });
+        }
+    }
+}
